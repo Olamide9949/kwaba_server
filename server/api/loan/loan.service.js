7030da554309207cda5272e05647aa08da1e7b20
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 11:02:42 -0400

    Updates!!!

--- server/api/loan/loan.service.js
+++ server/api/loan/loan.service.js
@@ -12,42 +12,38 @@ module.exports = {
         let obj = {
             loans: []
          };
+         let message = 'You have pending loan.';
+          
          let json = JSON.stringify(obj);
          let uuidV4 = Uuid.v4()  
          let new_id = uuidV4;
 
-         fs.readFile('loans.json', 'utf8', function readFileCallback(err, res){
-             if (err){
-                 return callBack(null, err);
-                } else {
-                    obj = JSON.parse(res);
-                    obj.loans.push({id:new_id, rent_status:data.rentStatus, loan_amount:data.rentAmount, customer_salary:data.salary, tenure:data.tenure});
-                    json = JSON.stringify(obj);
-                    fs.writeFile('loans.json', json, 'utf8', callBack);
-        }});
-        let response = {
-            id:new_id,
-            details:data
-        }
-        return callBack(null, response);
-    },
-    getLoan: (id, callBack) => {
-        [id]
-        fs.readFile('loans.json', 'utf8', function readFileCallback(err, res){
+         fs.readFile('loans.json', 'utf8', function readFileCallback(err, res){ 
             if (err){
                 return callBack(null, err);
             }
             obj = JSON.parse(res);
             if(obj.loans.length !==0){
-                let loan = obj.loans.filter((data)=>{
-                    if(data.id == id){
-                        return true;
-                    }
-                })
-                return callBack(null, loan);
+                return callBack(null, message);
             }else{
-                return callBack(null, 'You have no pending loan')
+                obj.loans.push({id:new_id, rent_status:data.rentStatus, loan_amount:data.rentAmount, customer_salary:data.salary, tenure:data.tenure});
+                json = JSON.stringify(obj);
+                fs.writeFile('loans.json', json, 'utf8', callBack);
             }
+            let response = {
+                id:new_id,
+                details:data
+            }
+            return callBack(null, response);
+        });
+    },
+    getLoan: callBack => {
+        fs.readFile('loans.json', 'utf8', function readFileCallback(err, res){
+            if (err){
+                return callBack(null, err);
+            }
+            obj = JSON.parse(res);
+            return callBack(null, obj)
         });
     }
 };
