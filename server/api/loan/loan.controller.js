98003a006ac04f88a92d33e6420f9009cd6f7340
Olamide Tosin <larryTosn@gmail.com>
2022-04-11 11:01:50 -0400

    Updates!!!

--- server/api/loan/loan.controller.js
+++ server/api/loan/loan.controller.js
@@ -20,33 +20,17 @@ module.exports = {
         });
     },
     getLoan: (req, res) => {
-        const ReqID = req.params.id;
-        getLoan(ReqID, (err, results) => {
+        getLoan((err, results) => {
             if(err) {
                 return res.status(500).json({
                     success: false,
                     message: `Oops ${err.messsage}`
                 });
             }
-
-            const interest = (parseInt(results[0].loan_amount) * (2 * 0.01)) / parseInt(results[0].tenure);
-            let payment = ((parseInt(results[0].loan_amount) / parseInt(results[0].tenure)) + interest).toFixed(2);
-            if(payment !=="NaN"){
-                return res.status(201).json({
-                    success: true,
-                    data:{
-                        id:results[0].id,
-                        approved_amount:results[0].loan_amount,
-                        monthly_payment:payment,
-                        tenure:results[0].tenure
-                    }
-                });
-            }else{
-                return res.status(202).json({
-                    success:true,
-                    message:'You have no pending loan'
-                });
-            }
+            return res.status(202).json({
+                success:true,
+                data:results
+            });
         });
     }
 }
